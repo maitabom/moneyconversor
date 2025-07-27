b2c819cc117fa016f469be376d5946ca9e5271c8
Fábio Valentim <maitabom@gmail.com>
2025-07-27 17:59:54 -0300

    Refatora o componente App para incluir o PickerItem e remove dependências não utilizadas

--- App.tsx
+++ App.tsx
@@ -5,16 +5,16 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { PickerItem } from './src/components/picker';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <View style={styles.currencyArea}>
+        <Text style={styles.title}>Selecione a sua moeda</Text>
+        <PickerItem />
+      </View>
     </View>
   );
 }
@@ -22,6 +22,23 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#101215',
+    paddingTop: 40,
+    alignItems: 'center',
+  },
+  currencyArea: {
+    backgroundColor: '#F9F9F9',
+    width: '90%',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    padding: 8,
+  },
+  title: {
+    fontSize: 16,
+    color: '#000',
+    fontWeight: '500',
+    paddingLeft: 5,
+    paddingTop: 5,
   },
 });
 
