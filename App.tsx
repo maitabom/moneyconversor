6bd79cb86e0784484e8b623049d74b9c91dc9942
Fábio Valentim <maitabom@gmail.com>
2025-08-02 20:00:10 -0300

    Adiciona lógica de conversão de moeda e exibição do resultado na aplicação

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  Keyboard,
 } from 'react-native';
 import { PickerItem } from './src/components/picker';
 import { api } from './src/services/api';
@@ -25,6 +26,10 @@ function App() {
   const [currencyPickerSelect, setCurrencyPickerSelect] =
     useState<CurrencyPicker | null>(null);
   const [loading, setLoading] = useState(true);
+
+  const [valueCurrency, setValueCurrency] = useState<number | null>(null);
+  const [convertedValue, setConvertedValue] = useState(0);
+  const [valueBCurrency, setValueBCurrency] = useState('');
 
   useEffect(() => {
     async function loadCurrency() {
@@ -46,6 +51,23 @@ function App() {
     loadCurrency();
     setLoading(false);
   }, []);
+
+  async function convert() {
+    
+
+    const response = await api.get(`app/${currencyPickerSelect?.key}-BRL`);
+
+    let result =
+      parseFloat(response.data[currencyPickerSelect!.key].ask) *
+      parseFloat(valueBCurrency);
+
+    console.log(result);
+
+    setConvertedValue(result);
+    setValueCurrency(parseFloat(valueBCurrency));
+
+    Keyboard.dismiss();
+  }
 
   if (loading) {
     return (
@@ -70,12 +92,34 @@ function App() {
         <Text style={styles.title}>
           Digite o valor para converter em real (R$).
         </Text>
-        <TextInput keyboardType='numeric' style={styles.input} placeholder="Exemplo 1.50" />
+        <TextInput
+          keyboardType="numeric"
+          style={styles.input}
+          placeholder="Exemplo 1.50"
+          value={valueBCurrency}
+          onChangeText={value => setValueBCurrency(value)}
+        />
       </View>
 
-      <TouchableOpacity style={styles.buttonArea}>
+      <TouchableOpacity style={styles.buttonArea} onPress={convert}>
         <Text style={styles.buttonText}>Converter</Text>
       </TouchableOpacity>
+
+      {convertedValue !== 0 && (
+        <View style={styles.resultArea}>
+          <Text style={styles.convertedValue}>
+            {valueCurrency} {currencyPickerSelect?.value}
+          </Text>
+          <Text style={styles.correspond}>corresponde a</Text>
+          <Text style={styles.convertedValue}>
+            {' '}
+            {convertedValue.toLocaleString('pt-br', {
+              style: 'currency',
+              currency: 'BRL',
+            })}{' '}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -98,13 +142,33 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
     padding: 8,
-    marginBottom: 1
+    marginBottom: 1,
   },
   valueArea: {
     backgroundColor: '#F9F9F9',
     width: '90%',
     paddingTop: 8,
-    paddingBottom: 8
+    paddingBottom: 8,
+  },
+  resultArea: {
+    width: '90%',
+    backgroundColor: '#FFF',
+    marginTop: 34,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  convertedValue: {
+    fontSize: 28,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  correspond: {
+    fontSize: 18,
+    margin: 8,
+    fontWeight: '500',
+    color: '#000',
   },
   buttonArea: {
     width: '90%',
@@ -113,13 +177,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderBottomLeftRadius: 8,
-    borderBottomRightRadius: 8
+    borderBottomRightRadius: 8,
   },
   input: {
     width: '100%',
     padding: 8,
     fontSize: 18,
-    color: '#000'
+    color: '#000',
   },
   title: {
     fontSize: 16,
@@ -131,8 +195,8 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#000',
     fontWeight: 'bold',
-    fontSize: 16
-  }
+    fontSize: 16,
+  },
 });
 
 export default App;
